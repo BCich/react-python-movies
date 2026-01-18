7f4e02b9966f6f5b15bc76cd701501338fb7530f
Bartlomiej <bcich@student.agh.edu.pl>
2026-01-18 13:43:04 +0100

    Inital commit

--- ui/src/App.js
+++ ui/src/App.js
@@ -8,9 +8,16 @@ function App() {
     const [movies, setMovies] = useState([]);
     const [addingMovie, setAddingMovie] = useState(false);
 
-    function handleAddMovie(movie) {
-        setMovies([...movies, movie]);
-        setAddingMovie(false);
+    async function handleAddMovie(movie) {
+        const response = await fetch('/movies', {
+            method: 'POST',
+            body: JSON.stringify(movie),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            setMovies([...movies, movie]);
+            setAddingMovie(false);
+        }
     }
 
     return (
@@ -19,11 +26,11 @@ function App() {
             {movies.length === 0
                 ? <p>No movies yet. Maybe add something?</p>
                 : <MoviesList movies={movies}
-                              onDeleteMovie={(movie) => setMovies(movies.filter(m => m !== movie))}
+                    onDeleteMovie={(movie) => setMovies(movies.filter(m => m !== movie))}
                 />}
             {addingMovie
                 ? <MovieForm onMovieSubmit={handleAddMovie}
-                             buttonLabel="Add a movie"
+                    buttonLabel="Add a movie"
                 />
                 : <button onClick={() => setAddingMovie(true)}>Add a movie</button>}
         </div>
